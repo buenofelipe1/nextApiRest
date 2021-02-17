92bd6046eaa6a151abbf9ee820506099446f4266
buenofelipe1 <49288188+buenofelipe1@users.noreply.github.com>
2021-02-17 08:58:53 -0300

    comit2

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,17 @@
-function tempo(request, response) {
+ async function tempo(request, response) {
+   
     const dynamicDate = new Date();
 
+    const subscribeData = await fetch("https://picsum.photos/v2/list");
+    const subscribeJson = await subscribeData.json();
+    const Lista = subscribeJson;
+    
+
     response.json({
-        date: dynamicDate.toGMTString()
-    })
+       date: dynamicDate.toGMTString(),
+       lista: Lista
+        
+    });
 }
 
 export default tempo;
